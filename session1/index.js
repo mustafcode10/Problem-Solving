853ef51408537c6899f679ca2b3ada10d587b497
mustafcode10 <mustafdiiriye@hotmail.com>
2019-11-26 15:37:17 +0200

    seekAndDestroy

--- session1/index.js
+++ session1/index.js
@@ -1,7 +1,5 @@
 
 
-
-
 // function addAll(){
 //  var args = Array.prototype.slice.call(arguments);
 //  var total = 0;
@@ -112,6 +110,23 @@
 // CHALLENGE 3: SEEK & DESTROY
 // Remove from the array whatever is in the following arguments. Return the leftover values in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+
+// Solution 1: arguments, indexOf, filter
+function seekAndDestroy(arr) {
+    const args = Array.from(arguments);
+  
+    function filterArr(arr) {
+      // Return true if NOT in array
+      return args.indexOf(arr) === -1;
+    }
+  
+    return arr.filter(filterArr);
+  }
+  
+  // Solution 2: ...rest, filter & includes
+  function seekAndDestroy(arr, ...rest) {
+    return arr.filter(val => !rest.includes(val));
+  }
 
 console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6))
 
